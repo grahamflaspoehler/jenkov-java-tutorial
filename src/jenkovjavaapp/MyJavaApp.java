92327ec5d3c8d0f1f94e8595b3308dc43accebb0
unknown <graham.flaspoehler@paradyme.us>
2020-05-13 21:25:32 -0400

    Using method as condition in if-statement

--- src/jenkovjavaapp/MyJavaApp.java
+++ src/jenkovjavaapp/MyJavaApp.java
@@ -2,28 +2,26 @@ package jenkovjavaapp;
 
 import java.util.Arrays;
 
-// + Switch statements allow you to choose between multiple code blocks depending on the value of a variable
-// + Provide the switch statement a value to switch on
-  // + each case statement compares the value of the switch with a constant, if equal, the block after the colon is
-// executed
-// the `break` keyword breaks you out of the switch statement, otherwise code would continue to execute down the rest
-// of the case statements
-// the default case is executed if none of the cases are met. 
-
 
 public class MyJavaApp {
   public static void main(String[] args) {
-    int amount = 10;
 
-    switch(amount) {
-      case 0 :
-        System.out.println("amount is 0"); break;
-      case 5 :
-        System.out.println("amount is 5"); break;
-      case 10 :
-        System.out.println("amount is 10"); break;
-      default :
-        System.out.println("amount is something else");
+    methodOne("1234");
+
+  }
+
+  public static void methodOne(String input) {
+    if ( isValid(input) ) {
+      System.out.println(input + " is valid");
+    } else {
+      System.out.println(input + " isn't valid");
     }
+  }
+
+  public static boolean isValid(String value) {
+    if ( value.equals("123") ) {
+      return true;
+    }
+    return false;
   }
 }
