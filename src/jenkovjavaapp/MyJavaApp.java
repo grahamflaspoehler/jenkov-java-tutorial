09574340fb28cc892e22867f3dac37b91fd5fde2
unknown <graham.flaspoehler@paradyme.us>
2020-05-13 20:52:06 -0400

    Switch statement

--- src/jenkovjavaapp/MyJavaApp.java
+++ src/jenkovjavaapp/MyJavaApp.java
@@ -2,15 +2,28 @@ package jenkovjavaapp;
 
 import java.util.Arrays;
 
+// + Switch statements allow you to choose between multiple code blocks depending on the value of a variable
+// + Provide the switch statement a value to switch on
+  // + each case statement compares the value of the switch with a constant, if equal, the block after the colon is
+// executed
+// the `break` keyword breaks you out of the switch statement, otherwise code would continue to execute down the rest
+// of the case statements
+// the default case is executed if none of the cases are met. 
+
+
 public class MyJavaApp {
   public static void main(String[] args) {
-//    Java if statements make a decisions between which of two blocks of code to execute. Here is a simple example:
-  int amount = 9;
+    int amount = 10;
 
-  if(amount > 9){
-    System.out.println("amount is greater than 9");
-  } else {
-    System.out.println("amount is 9 or less");
-  }
+    switch(amount) {
+      case 0 :
+        System.out.println("amount is 0"); break;
+      case 5 :
+        System.out.println("amount is 5"); break;
+      case 10 :
+        System.out.println("amount is 10"); break;
+      default :
+        System.out.println("amount is something else");
+    }
   }
 }
