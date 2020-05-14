488b700795715c0ab0435e7f5247772620f0322c
unknown <graham.flaspoehler@paradyme.us>
2020-05-13 20:41:01 -0400

    Example of if-statement

--- src/jenkovjavaapp/MyJavaApp.java
+++ src/jenkovjavaapp/MyJavaApp.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 
 public class MyJavaApp {
   public static void main(String[] args) {
-   
+//    Java if statements make a decisions between which of two blocks of code to execute. Here is a simple example:
+  int amount = 9;
+
+  if(amount > 9){
+    System.out.println("amount is greater than 9");
+  } else {
+    System.out.println("amount is 9 or less");
+  }
   }
 }
